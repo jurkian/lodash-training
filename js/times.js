94ac48e06859d27ee72da4d610bbd203511b93d1
Jakub Jurkian <jakub@jurkian.com>
2017-05-20 13:07:41 +0200

    Add times function

---
+++ js/times.js
@@ -0,0 +1,14 @@
+// Times
+
+let arr = [1, 13.49, 12, 5, 132.2421, 130, 44.5];
+let result;
+
+// Lodash
+_.times(5, () => {
+   console.log('_.times()');
+});
+
+// Native
+for (let i = 0; i < 5; i++) {
+   console.log('for');
+}
